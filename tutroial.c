b9ac19a9f9d8908a51a8af8ae2ffbf1f86a9e84a
RiccardoDAndrea <riccardo.dandrea@live.de>
2024-05-24 17:10:42 +0200

    chore: Add VSCode configuration files for ARM toolchain and debugging

---
+++ tutroial.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  
+#include <stdlib.h>
+
+int main() {
+    printf("There once was a man named");
+    printf("He was years old");
+
+    return 0;  // Füge diese Zeile hinzu, um die main-Funktion korrekt abzuschließen
+}
