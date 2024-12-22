f8ddb2eff49d3b39a566c2e25dbdb2a78886ed99
RiccardoDAndrea <riccardo.dandrea@live.de>
2024-12-22 09:34:35 +0100

    Deutscher Text

--- tutroial.cpp
+++ tutroial.cpp
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#inclu>e <stdio.h>
 #include <iostream>
 
 int main() {
     std::cout <<  "Hello, World!";
+    std::count << "Guten Tag"
     return 0;  // Hauptfunktion korrekt abschließen
 }
 
