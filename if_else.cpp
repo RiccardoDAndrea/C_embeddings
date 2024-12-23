ef9103d7a84d3b3088992af68ac0c485bf034796
RiccardoDAndrea <riccardo.dandrea@live.de>
2024-12-23 14:57:28 +0100

    spnaische worte

--- if_else.cpp
+++ if_else.cpp
@@ -1,1 +1,28 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Vokalebtrainer\n";
+    cout << "Bitte treffe eine Auswahl\n";
+    cout << "(1) Neue Vokabel hinzufuegen\n";
+    cout << "(2) Vokabeltest starten\n";
+    cout << "(3) Bestanden";
+    string option;
+    cin >> option;
+
+    if (option == "1") {
+        cout << "Welche Vokabel (Deutsch)?";
+        string german_word;
+        string spanish_word;
+        cin >> german_word;
+        cout << "Wie lautet die Uebersetzung?";
+        cin >> spanish_word;
+        cout << german_word + "lautet auf Spanisch" +
+          spanish_word;
+
+
+        } else {
+          cout << "Vokabeltest wird gestartet...";
+        }
+}
