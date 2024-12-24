a54df2b60c5aa95115ae35a1c77c22b03118d573
RiccardoDAndrea <riccardo.dandrea@live.de>
2024-12-24 14:41:37 +0100

    learnning Datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+#include <string>
+// Ein Zeilige Datentypen
+
+/*
+Das ist eine Skript fuer die Datentypen
+*/ 
+
+// int    - stores integers (whole numbers), without decimals, suchs as 123 or -123
+// double - stores folating point numbers, with decimals, such as 19.99 or -19.99
+// char   - stores single charakters, such as "a" or "b". Char values are surrounded by single quotes
+// string - stores text, such as "Hello World". String values are surrounded by double quotes
+// bool   - stores values witht two states: true or false
+
+int main(){
+// studenten Daten
+
+  int studentID = 15;
+  int studentAge = 23;
+  float studentFee = 75.25;
+  string department = "Wiso";
+  char studentGrade = 'B';
+  
+  cout << "Student ID: " << studentID << "\n";
+  cout << "Studenten Age: " << studentAge << "\n";
+  cout << "Studentfee: " << studentFee << "\n";
+  cout << "Department: " << department << "\n";
+  cout << "Studengrade: " << studentGrade << "\n";
+
+
+  return 0;
+
+
+// Print Variables
+
+
+
+
+}
