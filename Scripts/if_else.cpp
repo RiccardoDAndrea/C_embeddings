041210268c133e96e84684307cba3b69fc7b344a
RiccardoDAndrea <riccardo.dandrea@live.de>
2025-03-30 20:52:04 +0200

    refactor if_else.cpp and add tellmeastory.cpp for improved game interaction

--- Scripts/if_else.cpp
+++ Scripts/if_else.cpp
@@ -9,58 +9,56 @@ int main() {
     cout << "(y)es \n";
     cout << "(n)o";
 
-    string option;      // string  the Hunted House which room do you want to enter? \n";
-        cout << "(B)edroom \n";
-        cout << "(D)ark room\n";
-        cout << "(E)mpty room\n";
+    // string  the Hunted House which room do you want to enter? \n";
+    cout << "(B)edroom \n";
+    cout << "(D)ark room\n";
+    cout << "(E)mpty room\n";
 
-        string answer_1;
-        cin >> answer_1;
+    string answer_1;
+    cin >> answer_1;
 
-        if (answer_1 == "B"){
-            cout << "Bedroom\n";
-            cout << "The cupboard is locked with a digit pin\n";
-            cout << "Enter a random digit code ! \n";
+    if (answer_1 == "B"){
+        cout << "Bedroom\n";
+        cout << "The cupboard is locked with a digit pin\n";
+        cout << "Enter a random digit code ! \n";
 
-            int digit_code;
-            cin >> digit_code;
-            
-            if (digit_code == 0000){
-              cout << "You have sucessfully entered the right combination" << digit_code;
-              }
-            else {
-              cout << ("Nothing to see");
-            };
-        } else if (answer_1 == "D") {
-            cout << "You have entered the Dark room\n";
-            cout << "You see a myseries looking Chest do you try to open it? \n";
-            cout << "(y)es \n";
-            cout << "(n)o\n";
-            string chest_open;
-            cin >> chest_open;
-
-
-
-            if (chest_open == "y"){
-              cout << "You open the chest a see a myseries medall from the second world war 2 with the name Charles Hunt\n";
-            }
-            
-
-
-
-        } else if (answer_1 == "E"){
-
-            cout << "Your entering a empty room\n";
-            cout << "A Dark figure is looking at you ?\n";
-            cout << "Do you want to attack him\n";
-            cout << "(y)es\n";
-            cout << "(n)o \n";
-            string combat;
-            cin >> combat;
-
-
+        int digit_code;
+        cin >> digit_code;
+        
+        if (digit_code == 0000){
+          cout << "You have sucessfully entered the right combination" << digit_code;
+          }
+        else {
+          cout << ("Nothing to see");
         };
-        } else {
-          cout << "Good luck next time";
-        };
+    } else if (answer_1 == "D") {
+        cout << "You have entered the Dark room\n";
+        cout << "You see a myseries looking Chest do you try to open it? \n";
+        cout << "(y)es \n";
+        cout << "(n)o\n";
+        string chest_open;
+        cin >> chest_open;
+
+
+
+        if (chest_open == "y"){
+          cout << "You open the chest a see a myseries medall from the second world war 2 with the name Charles Hunt\n";
+        }
+        
+
+
+
+    } else if (answer_1 == "E"){
+
+        cout << "Your entering a empty room\n";
+        cout << "A Dark figure is looking at you ?\n";
+        cout << "Do you want to attack him\n";
+        cout << "(y)es\n";
+        cout << "(n)o \n";
+        string combat;
+        cin >> combat;
+
+
+    };
+    
 }
---
+++ Scripts/tellmeastory.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    cout << "Welcome to the game Faith \n";
+    cout << "If you wont to proceed enter your age \n";
+    cout << "Age: ";
+    int age;
+    cin >> age;
+    if (age > 18){
+        cout << "Oh great your are old enough \n";
+        cout << "Have you ever asked your self ";
+        string answer_possibilites = "(y)es or (n)o";}
+
+        if(answer_possibilites){
+            cout << ""
+        }
+    else {
+        cout << "Well its look likes if you are not old enough come back when you als over 18 \n";}
+}
